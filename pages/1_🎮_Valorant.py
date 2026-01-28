3ce0ab379b2eab7f87eb29bfc4edeb6a21be11e4
DavinaParadoxe <133243824+DavinaCondorcet@users.noreply.github.com>
2026-01-28 16:41:16 +0100

    Add Valorant Tracker using Streamlit

---
+++ pages/1_🎮_Valorant.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from valorant.database import init_db
+from valorant.updater import update_matches, update_rank
+from valorant.plots import kda_over_time, winrate_by_agent
+
+RIOT_ID = "YukiBloo"
+TAG = "EUW"
+
+st.title("🎮 Valorant Tracker")
+
+init_db()
+
+with st.spinner("Sync Valorant data…"):
+    update_matches(RIOT_ID, TAG)
+    update_rank(RIOT_ID, TAG)
+
+st.plotly_chart(kda_over_time(), use_container_width=True)
+st.plotly_chart(winrate_by_agent(), use_container_width=True)
